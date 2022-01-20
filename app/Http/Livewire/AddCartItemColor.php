684828bf496646d82016392796e72435b38f1226
joseafm93 <joseafm93@gmail.com>
2022-01-20 12:52:26 +0000

    Fin paso 60

--- app/Http/Livewire/AddCartItemColor.php
+++ app/Http/Livewire/AddCartItemColor.php
@@ -2,16 +2,54 @@
 
 namespace App\Http\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class AddCartItemColor extends Component
 {
     public $product;
     public $colors;
+    public $qty = 1;
+    public $quantity = 0;
+    public $color_id = '';
+    public $options = [];
 
     public function mount()
     {
         $this->colors = $this->product->colors;
+        $this->options['image'] = Storage::url($this->product->images->first()->url);
+    }
+
+    public function updatedColorId($value)
+    {
+        $color = $this->product->colors->find($value);
+        $this->quantity = $color->pivot->quantity;
+        $this->options['color'] = $color->name;
+    }
+
+    public function decrement()
+    {
+        $this->qty--;
+    }
+
+    public function increment()
+    {
+        $this->qty++;
+    }
+
+    public function addItem()
+    {
+        Cart::add([
+            'id' => $this->product->id,
+            'name' => $this->product->name,
+            'qty' => $this->qty,
+            'price' => $this->product->price,
+            'weight' => 550,
+            'options' => $this->options,
+        ]);
+
+        $this->emitTo('dropdown-cart', 'render');
     }
 
     public function render()
