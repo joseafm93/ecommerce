4a10ca7c546925b4b37127e0eac34bf856394438
joseafm93 <joseafm93@gmail.com>
2022-01-20 13:06:46 +0000

    Fin paso 61 completo

--- resources/views/livewire/add-cart-item-color.blade.php
+++ resources/views/livewire/add-cart-item-color.blade.php
@@ -1,12 +1,25 @@
 <div x-data>
-    <p class="text-xl text-gray-700">Color:</p>
+    <div>
+        <p class="text-xl text-gray-700">Talla:</p>
+        <select wire:model="size_id" class="form-control w-full">
+            <option value="" selected disabled>Seleccione una talla</option>
+            @foreach ($sizes as $size)
+                <option value="{{$size->id}}">{{$size->name}}</option>
+            @endforeach
+        </select>
+    </div>
 
-    <select wire:model="color_id" class="form-control w-full">
-        <option value="" selected disabled>Seleccionar un color</option>
-        @foreach ($colors as $color)
-            <option value="{{$color->id}}">{{  __(ucfirst($color->name)) }}</option>
-        @endforeach
-    </select>
+    <div class="mt-2">
+        <p class="text-xl text-gray-700">Color:</p>
+
+        <select wire:model="color_id" class="form-control w-full">
+            <option value="" selected disabled>Seleccione un color</option>
+
+            @foreach ($colors as $color)
+                <option value="{{$color->id}}">{{ __(ucfirst($color->name)) }}</option>
+            @endforeach
+        </select>
+    </div>
 
     <div class="flex mt-4">
         <div class="mr-4">
