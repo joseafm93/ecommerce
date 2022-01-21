710781faf8b4f8edbe6ab23ec4e7b9e000c576e9
joseafm93 <joseafm93@gmail.com>
2022-01-21 18:16:19 +0000

    Fin paso 79

--- app/Http/Livewire/ShoppingCart.php
+++ app/Http/Livewire/ShoppingCart.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class ShoppingCart extends Component
@@ -11,5 +12,12 @@ class ShoppingCart extends Component
     public function render()
     {
         return view('livewire.shopping-cart');
+    }
+
+    public function destroy()
+    {
+        Cart::destroy();
+
+        $this->emitTo('dropdown-cart', 'render');
     }
 }
