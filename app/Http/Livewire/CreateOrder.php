80b3ab00d32445fe5781f77b3db5fee30f8ff30b
joseafm93 <joseafm93@gmail.com>
2022-01-22 16:44:44 +0000

    Fin paso 95

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -3,12 +3,14 @@
 namespace App\Http\Livewire;
 
 use App\Models\Department;
+use App\Models\Order;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Livewire\Component;
 
 class CreateOrder extends Component
 {
     public $envio_type = 1;
-    public $contact, $phone;
+    public $contact, $phone, $shipping_cost;
     public $address, $reference;
     public $departments, $cities = [], $districts = [];
     public $department_id = '', $city_id = '', $district_id = '';
@@ -24,9 +26,13 @@ class CreateOrder extends Component
         $this->departments = Department::all();
     }
 
-    public function render()
+    public function updatedEnvioType($value)
     {
-        return view('livewire.create-order');
+        if ($value == 1) {
+            $this->resetValidation([
+                'department_id', 'city_id', 'district_id', 'address', 'reference',
+            ]);
+        }
     }
 
     public function create_order()
@@ -40,14 +46,26 @@ class CreateOrder extends Component
             $rules['reference'] = 'required';
         }
         $this->validate($rules);
+
+        $order = new Order();
+
+        $order->user_id = auth()->user()->id;
+        $order->contact = $this->contact;
+        $order->phone = $this->phone;
+        $order->envio_type = $this->envio_type;
+        $order->shipping_cost = 0;
+        $order->total = $this->shipping_cost + Cart::subtotal();
+        $order->content = Cart::content();
+
+        $order->save();
+
+        Cart::destroy();
+
+        return redirect()->route('orders.payment', $order);
     }
 
-    public function updatedEnvioType($value)
+    public function render()
     {
-        if ($value == 1) {
-            $this->resetValidation([
-                'department_id', 'city_id', 'district_id', 'address', 'reference',
-            ]);
-        }
+        return view('livewire.create-order');
     }
 }
