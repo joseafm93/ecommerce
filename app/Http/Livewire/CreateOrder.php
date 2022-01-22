54e96c79e6431bba01f7a0d4366ccc62f0c9a2ce
joseafm93 <joseafm93@gmail.com>
2022-01-22 16:32:38 +0000

    Fin paso 94

--- app/Http/Livewire/CreateOrder.php
+++ app/Http/Livewire/CreateOrder.php
@@ -41,4 +41,13 @@ class CreateOrder extends Component
         }
         $this->validate($rules);
     }
+
+    public function updatedEnvioType($value)
+    {
+        if ($value == 1) {
+            $this->resetValidation([
+                'department_id', 'city_id', 'district_id', 'address', 'reference',
+            ]);
+        }
+    }
 }
