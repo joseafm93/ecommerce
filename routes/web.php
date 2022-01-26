2653ebc3c2754b52d708cde88660939dab9f1687
joseafm93 <joseafm93@gmail.com>
2022-01-26 17:24:55 +0000

    Fin paso 109

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Livewire\CreateOrder;
+use App\Http\Livewire\PaymentOrder;
 use App\Http\Livewire\ShoppingCart;
 use Illuminate\Support\Facades\Route;
 
@@ -32,4 +33,6 @@ Route::get('search', SearchController::class)->name('search');
 
 Route::get('shopping-cart', ShoppingCart::class)->name('shopping-cart');
 
-Route::get('orders/{order}/payment', [OrderController::class, 'payment'])->name('orders.payment');
+Route::get('orders/{order}/payment', PaymentOrder::class)->name('orders.payment');
+
+Route::get('orders/{order}', [OrderController::class, 'show'])->name('orders.show');
