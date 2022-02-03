d8bee3094fe50a3ef257c1f523d2c3a0ccc76f78
joseafm93 <joseafm93@gmail.com>
2022-02-03 11:36:48 +0000

    Fin paso 124

--- routes/admin.php
+++ routes/admin.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowProducts::class)->name('admin.index');
 
+Route::get('products/create', function (){})->name('admin.products.create');
 Route::get('products/{product}/edit', function () {})->name('admin.products.edit');
