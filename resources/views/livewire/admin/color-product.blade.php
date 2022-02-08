95ae2a2da6583f922670a514a3571b96d6d3664b
joseafm93 <joseafm93@gmail.com>
2022-02-08 15:54:35 +0000

    Fin paso 142

--- resources/views/livewire/admin/color-product.blade.php
+++ resources/views/livewire/admin/color-product.blade.php
@@ -16,66 +16,59 @@
             </div>
             <x-jet-input-error for="color_id" />
         </div>
+        <div>
+            <x-jet-label>
+                Cantidad
+            </x-jet-label>
+            <x-jet-input type="number" wire:model.defer="quantity" placeholder="Ingrese una cantidad"
+                         class="w-full" />
+            <x-jet-input-error for="quantity" />
+        </div>
+        <div class="flex justify-end items-center mt-4">
+            <x-jet-action-message class="mr-3" on="saved">
+                Agregado
+            </x-jet-action-message>
+            <x-jet-button wire:loading.attr="disabled" wire:target="save" wire:click="save">
+                Agregar
+            </x-jet-button>
+        </div>
     </div>
-
-    <div>
-        <x-jet-label>
-            Cantidad
-        </x-jet-label>
-        <x-jet-input type="number" wire:model.defer="quantity" placeholder="Ingrese una cantidad" class="w-full" />
-        <x-jet-input-error for="quantity" />
-    </div>
-
-    <div class="flex justify-end items-center mt-4">
-        <x-jet-action-message class="mr-3" on="saved">
-            Agregado
-        </x-jet-action-message>
-        <x-jet-button wire:loading.attr="disabled" wire:target="save" wire:click="save">
-            Agregar
-        </x-jet-button>
-    </div>
-
-    @if($productColors->count())
+    @if ($productColors->count())
         <div class="bg-white shadow-lg rounded-lg p-6">
             <table>
                 <thead>
-                <tr>
-                    <th class="px-4 py-2 w-1/3">
-                        Color
-                    </th>
-                    <th class="px-4 py-2 w-1/3">
-                        Cantidad
-                    </th>
-                    <th class="px-4 py-2 w-1/3">
-
-                    </th>
-                </tr>
+                    <tr>
+                        <th class="px-4 py-2 w-1/3">
+                            Color
+                        </th>
+                        <th class="px-4 py-2 w-1/3">
+                            Cantidad
+                        </th>
+                        <th class="px-4 py-2 w-1/3">
+                        </th>
+                    </tr>
                 </thead>
-
                 <tbody>
-                    @foreach($productColors as $color)
-                        <tr wire:key="product_color-{{ $color->pivot->id }}">
-                            <td class="capitalize px-4 py-2">
-                                {{ __(ucfirst($colors->find($color->pivot->color_id)->name)) }}
-                            </td>
-                            <td class="px-4 py-2">
-                                {{ $color->pivot->quantity }} unidades
-                            </td>
-                            <td class="px-4 py-2 flex">
-                                <x-jet-secondary-button
-                                    class="ml-auto mr-2"
-                                    wire:click="edit({{ $color->pivot->id }})"
-                                    wire:loading.attr="disabled"
-                                    wire:target="edit({{ $color->pivot->id }})">
-                                    Actualizar
-                                </x-jet-secondary-button>
-                                <x-jet-danger-button
-                                    wire:click="$emit('deletePivot', {{ $color->pivot->id }})">
-                                    Eliminar
-                                </x-jet-danger-button>
-                            </td>
-                        </tr>
-                    @endforeach
+                @foreach ($productColors as $color)
+                    <tr wire:key="product_color-{{ $color->pivot->id }}">
+                        <td class="capitalize px-4 py-2">
+                            {{ __(ucfirst($colors->find($color->pivot->color_id)->name)) }}
+                        </td>
+                        <td class="px-4 py-2">
+                            {{ $color->pivot->quantity }} unidades
+                        </td>
+                        <td class="px-4 py-2 flex">
+                            <x-jet-secondary-button class="ml-auto mr-2"
+                                                    wire:click="edit({{ $color->pivot->id }})" wire:loading.attr="disabled"
+                                                    wire:target="edit({{ $color->pivot->id }})">
+                                Actualizar
+                            </x-jet-secondary-button>
+                            <x-jet-danger-button wire:click="$emit('deleteColor', {{ $color->pivot->id }})">
+                                Eliminar
+                            </x-jet-danger-button>
+                        </td>
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
         </div>
@@ -115,6 +108,7 @@
         </x-slot>
     </x-jet-dialog-modal>
 </div>
+
 
 @push('scripts')
     <script>
