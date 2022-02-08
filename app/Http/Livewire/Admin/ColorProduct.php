1a13fce472086d2968116b4c07a7f3b8e86e3957
joseafm93 <joseafm93@gmail.com>
2022-02-08 15:00:34 +0000

    Fin paso 140

--- app/Http/Livewire/Admin/ColorProduct.php
+++ app/Http/Livewire/Admin/ColorProduct.php
@@ -31,6 +31,17 @@ class ColorProduct extends Component
         $this->pivot_quantity = $pivot->quantity;
     }
 
+    public function update()
+    {
+        $this->pivot->color_id = $this->pivot_color_id;
+        $this->pivot->quantity = $this->pivot_quantity;
+        $this->pivot->save();
+
+        $this->product = $this->product->fresh();
+        
+        $this->open = false;
+    }
+
     public function save() {
         $this->validate();
 
