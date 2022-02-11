596f6b4511846a5156f211cc9663c593e6190578
joseafm93 <joseafm93@gmail.com>
2022-02-11 10:25:06 +0000

    Fin paso 161

--- app/Http/Livewire/Admin/CreateCategory.php
+++ app/Http/Livewire/Admin/CreateCategory.php
@@ -10,7 +10,7 @@ use Livewire\WithFileUploads;
 
 class CreateCategory extends Component
 {
-    public $brands, $image;
+    public $brands, $categories, $image;
     public $createForm = [
         'name' => null,
         'slug' => null,
@@ -37,13 +37,18 @@ class CreateCategory extends Component
     public function mount()
     {
         $this->getBrands();
+        $this->getCategories();
         $this->image = 1;
-
     }
 
     public function getBrands()
     {
         $this->brands = Brand::all();
+    }
+
+    public function getCategories()
+    {
+        $this->categories = Category::all();
     }
 
     public function updatedCreateFormName($value)
@@ -67,7 +72,11 @@ class CreateCategory extends Component
         $category->brands()->attach($this->createForm['brands']);
 
         $this->image = 2;
+
         $this->reset('createForm');
+
+        $this->getCategories();
+        $this->emit('saved');
     }
 
     public function render()
