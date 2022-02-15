c768a3922896ee58c6e0c87f6e140707823db2d9
joseafm93 <joseafm93@gmail.com>
2022-02-15 15:27:56 +0000

    Fin paso 190

--- resources/views/livewire/payment-order.blade.php
+++ resources/views/livewire/payment-order.blade.php
@@ -1,34 +1,32 @@
 <div>
-    <div class="grid grid-cols-5 gap-6 container-menu py-8">
-        <div class="col-span-3">
+    <div class="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-5 gap-6 container-menu py-8">
+        <div class="order-2 lg:order-1 xl:col-span-3">
             <div class="bg-white rounded-lg shadow-lg px-6 py-4 mb-6">
-                <p class="text-gray-700 uppercase"><span class="font-semibold">Número de Orden:</span> {{ $order->id }}</p>
+                <p class="text-gray-700 uppercase"><span class="font-semibold">Número de Orden:</span>
+                    {{ $order->id }}
+                </p>
             </div>
-
             <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
                 <div class="grid grid-cols-2 gap-6">
                     <div>
                         <p class="text-lg font-semibold uppercase">Envío</p>
-
-                        @if($order->envio_type == 1)
+                        @if ($order->envio_type == 1)
                             <p class="text-sm">Los productos deben ser recogidos en tienda</p>
                             <p class="text-sm">Calle Falsa 123</p>
                         @else
                             <p class="text-sm">Los productos serán enviados a:</p>
                             <p class="text-sm">{{ $envio->address }}</p>
-                            <p>{{ $envio->department }} - {{ $envio->city }} - {{ $envio->district }}</p>
+                            <p>{{ $envio->department }} - {{ $envio->city }} -
+                                {{ $envio->district }}</p>
                         @endif
                     </div>
-
                     <div>
                         <p class="text-lg font-semibold uppercase">Datos de contacto</p>
-
                         <p class="text-sm">Persona que recibirá el producto: {{ $order->contact }}</p>
                         <p class="text-sm">Teléfono de contacto: {{ $order->phone }}</p>
                     </div>
                 </div>
             </div>
-
             <div class="bg-white rounded-lg shadow-lg p-6 text-gray-700">
                 <p class="text-xl font-semibold mb-4">Resumen</p>
                 <table class="table-auto w-full">
@@ -41,12 +39,12 @@
                     </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-200">
-                    @foreach($items as $item)
+                    @foreach ($items as $item)
                         <tr>
                             <td>
                                 <div class="flex">
-                                    <img class="h-15 w-20 object-cover mr-4"
-                                         src="{{ $item->options->image }}" alt="">
+                                    <img class="h-15 w-20 object-cover mr-4" src="{{ $item->options->image }}"
+                                         alt="">
                                     <article>
                                         <h1 class="font-bold">{{ $item->name }}</h1>
                                         <div class="flex text-xs">
@@ -74,49 +72,48 @@
                     </tbody>
                 </table>
             </div>
-
-            <div class="col-span-2">
-                <div class="bg-white rounded-lg shadow-lg px-6 pt-6">
-                    <div class="flex justify-between items-center mb-4">
-                        <img class="h-8" src="{{ asset('img/MC_VI_DI_2-1.jpg') }}" alt="">
-                        <div class="text-gray-700">
-                            <p class="text-sm font-semibold">
-                                Subtotal: {{ $order->total - $order->shipping_cost }} &euro;
-                            </p>
-                            <p class="text-sm font-semibold">
-                                Envío: {{ $order->shipping_cost }} &euro;
-                            </p>
-                            <p class="text-lg font-semibold uppercase">
-                                Pago: {{ $order->total }} &euro;
-                            </p>
-                        </div>
+        </div>
+        <div class="order-1 lg:order-2 xl:col-span-2">
+            <div class="bg-white rounded-lg shadow-lg p-6 px-6 pt-6">
+                <div class="flex justify-between items-center mb-4">
+                    <img class="h-8" src="{{ asset('img/MC_VI_DI_2-1.jpg') }}" alt="">
+                    <div class="text-gray-700">
+                        <p class="text-sm font-semibold">
+                            Subtotal: {{ $order->total - $order->shipping_cost }} &euro;
+                        </p>
+                        <p class="text-sm font-semibold">
+                            Envío: {{ $order->shipping_cost }} &euro;
+                        </p>
+                        <p class="text-lg font-semibold uppercase">
+                            Pago: {{ $order->total }} &euro;
+                        </p>
                     </div>
-
-                    <div id="paypal-button-container"></div>
                 </div>
+                <div id="paypal-button-container"></div>
             </div>
         </div>
     </div>
 
     @push('scripts')
-    <script src="https://www.paypal.com/sdk/js?client-id={{ config('services.paypal.client_id') }}&currency=EUR"></script>
-    <script>
-        paypal.Buttons({
-            createOrder: function(data, actions) {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: "{{ $order->total }}"
-                        }
-                    }]
-                });
-            },
-            onApprove: function(data, actions) {
-                return actions.order.capture().then(function(orderData) {
-                    Livewire.emit('payOrder');
-                });
-            }
-        }).render('#paypal-button-container');
-    </script>
+        <script src="https://www.paypal.com/sdk/js?client-id={{ config('services.paypal.client_id') }}&currency=EUR">
+        </script>
+        <script>
+            paypal.Buttons({
+                createOrder: function(data, actions) {
+                    return actions.order.create({
+                        purchase_units: [{
+                            amount: {
+                                value: "{{ $order->total }}"
+                            }
+                        }]
+                    });
+                },
+                onApprove: function(data, actions) {
+                    return actions.order.capture().then(function(orderData) {
+                        Livewire.emit('payOrder');
+                    });
+                }
+            }).render('#paypal-button-container');
+        </script>
     @endpush
 </div>
